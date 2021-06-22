8502d3d0400dc5569d70e273f24c7b2d2516f75a
Nikita Gupta <gnikita432@gmail.com>
2021-06-22 08:44:51 +0530

    Implemented the graph

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import InfoBox from "./InfoBox";
 import Map from "./Map";
 import Table from "./Table";
 import { sortData } from "./util";
+import LineGraph from "./LineGraph";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -103,8 +104,10 @@ function App() {
           {/* table  */}
           <h3>Live Cases by Country</h3>
           <Table countries={tableData}/>
+
           {/* graph  */}
           <h3>WorldWide new Cases</h3>
+          <LineGraph/>
         </CardContent>
       </Card>
     </div>
